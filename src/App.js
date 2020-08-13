b69c6dc9b4818a7bb88fa5e54ae65ec16d4ccd14
Charles Davidson <cddavidson86@yahoo.com>
2020-08-13 18:50:37 -0500

    insalled Axios. Preparing to request.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import Navbar from './components/layout/Navbar'
 import Users from "./components/users/Users"
 import './App.css';
 
+
+
 class App extends Component {
+componentDidMount() {
+  console.log(12345);  
+}
+
+
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Users />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -12,21 +12,21 @@ class Users extends Component {
             },
             {
                 id: '2',
-                login: 'mojombo',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+                login: 'Frank',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
                 html_url: 'https://github.com/mojombo'
             },
             {
                 id: '3',
-                login: 'mojombo',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+                login: 'Bill',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
                 html_url: 'https://github.com/mojombo'
             }
         ]
     }
     render() {
         return (
-            <div>
+            <div style={userStyle}>
                 {this.state.users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
@@ -34,5 +34,12 @@ class Users extends Component {
         )
     }
 }
+
+const userStyle = {
+    display: "grid",
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+}
+
 
 export default Users
