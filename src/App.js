c49188cd1413855ce6dd4238696b80935fbde70b
Charles Davidson <cddavidson86@yahoo.com>
2020-08-26 18:22:14 -0500

    Adding search callback function for searching API for users.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -10,15 +11,25 @@ class App extends Component {
 		loading: false
 	};
 
-	async componentDidMount() {
-		this.setState({ loading: true });
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
 
-		const res = await axios.get(
-			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process
+	// 			.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+	// 	);
+
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	// Search Github Users
+	searchUsers = async text => {
+    	const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process
 				.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
 
-		this.setState({ users: res.data, loading: false });
+		this.setState({ users: res.data.items, loading: false });
 	}
 
 	render() {
@@ -26,6 +37,7 @@ class App extends Component {
 			<div className="App">
 				<Navbar />
 				<div className="container">
+          		<Search searchUsers={this.searchUsers} />
 					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
