ad818a6140732080449cccf83b61dab3f2786401
Charles Davidson <cddavidson86@yahoo.com>
2020-09-28 02:01:45 -0500

    Creating a responsive CLEAR button that only shows up after a search has populated.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,9 @@ class App extends Component {
 
 	// Search Github Users
 	searchUsers = async text => {
-    	const res = await axios.get(
+		this.setState({ loading: true });
+
+		const res = await axios.get(
 			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process
 				.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
@@ -32,12 +34,15 @@ class App extends Component {
 		this.setState({ users: res.data.items, loading: false });
 	}
 
+	// Clear users from state
+	clearUsers = () => this.setState({ users: [], loading: false });
+
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
 				<div className="container">
-          		<Search searchUsers={this.searchUsers} />
+					<Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={ this.state.users.length > 0 ? true : false } />
 					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
