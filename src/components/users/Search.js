8793b0912ceef2942c879263b996ee896904f6d7
Charles Davidson <cddavidson86@yahoo.com>
2020-09-28 02:49:12 -0500

    Added a please enter something when the user searches without entering anything in the search.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,16 +10,21 @@ export class Search extends Component {
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
     }
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' });
-    }
+        if (this.state.text === '') {
+            this.props.setAlert("Please enter something", "light");
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        }
+    };
 
     render() {
         const { showClear, clearUsers } = this.props;
@@ -31,10 +36,10 @@ export class Search extends Component {
                     <input type="submit" value="search" className="btn btn-dark btn-block" />
                 </form>
 
-                { showClear &&
+                {showClear &&
                     (<button className="btn btn-light btn-block" onClick={clearUsers}>
                         Clear
-                 </button>
+                    </button>
                     )}
 
             </div>
